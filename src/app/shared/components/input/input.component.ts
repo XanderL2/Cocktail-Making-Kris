226f56d76cab85f2aeff2ad09c4960dded473cdc
XanderL2 <alexanderllave2021@gmail.com>
2025-05-16 23:08:36 +0200

    Fix: Reactive Forms

--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -2,15 +2,27 @@ import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { forwardRef } from '@angular/core';
 
 
 @Component({
   selector: 'app-input',
   imports: [ FormsModule, MatIconModule, CommonModule ],
   templateUrl: './input.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true
+    }
+  ],
   styleUrl: './input.component.scss'
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor {
+
+  private onChange: any = () => {};
+  private onTouched: any = () => {};
 
   @Input({required: true}) name!: string; 
   @Input({required: true}) ariaLabel!: string;
@@ -30,5 +42,22 @@ export class InputComponent {
 
   public emitValue() : void{
     this.getValue.emit({name: this.name, value: this.value});
+    this.onChange(this.value);
+    this.onTouched();
   }
+
+
+  writeValue(value: any): void {
+    this.value = value; 
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn; 
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn; 
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    throw new Error('Method not implemented.');
+  }
+  
 }
