0150835d2a363a07120576ca7311153923ab1a95
XanderL2 <alexanderllave2021@gmail.com>
2025-05-16 14:57:35 +0200

    Fix: Routes

--- src/app/shared/components/music-player/models/Songs.ts
+++ src/app/shared/components/music-player/models/Songs.ts
@@ -5,22 +5,22 @@ export const Songs: Song[] = [
     {
         name: "Every Day Is Night",
         artist: "Vallhalla",
-        cover: "/music/every-day/every-day.webp",
-        songUrl: "/music/every-day/EveryDay.mp3"
+        cover: "music/every-day/every-day.webp",
+        songUrl: "music/every-day/EveryDay.mp3"
     },
 
     {
         name: "Let There Be (Love)",
         artist: "Cidoku",
-        cover: "/music/let-there-be/let-there-be.jpg",
-        songUrl: "/music/let-there-be/let-there-be.mp3"
+        cover: "music/let-there-be/let-there-be.jpg",
+        songUrl: "music/let-there-be/let-there-be.mp3"
     },
 
     {
         name: "Quiet Air",
         artist: "Cidoku",
-        cover: "/music/quiet-air/quiet-air.png",
-        songUrl: "/music/quiet-air/quiet_air.mp3"
+        cover: "music/quiet-air/quiet-air.png",
+        songUrl: "music/quiet-air/quiet_air.mp3"
     },
 
 ]
