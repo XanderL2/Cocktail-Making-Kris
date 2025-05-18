a7f7d5dc538e5c64059620bca798aa78e131cb7e
XanderL2 <alexanderllave2021@gmail.com>
2025-05-18 11:04:13 +0200

    Feat: Base Login Form

--- src/app/features/auth/pages/login/login.component.ts
+++ src/app/features/auth/pages/login/login.component.ts
@@ -3,16 +3,27 @@ import { InputComponent } from "../../../../shared/components/input/input.compon
 import { ButtonComponent } from "../../../../shared/components/button/button.component";
 import { AuthFormComponent } from "../../components/auth-form/auth-form.component";
 import { Router } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [InputComponent, ButtonComponent, AuthFormComponent],
+  imports: [InputComponent, ButtonComponent, AuthFormComponent, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
   private router: Router = inject(Router);
+  public loginForm: FormGroup;
+
+
+  constructor(){
+    this.loginForm = new FormGroup({
+      username: new FormControl<string>("", [Validators.required, Validators.minLength(5)]),
+      password: new FormControl<string>("", [Validators.required, Validators.minLength(5)]),
+    });
+  }
+
 
   navigateToRegister = () => {
     this.router.navigate(['/register']);
